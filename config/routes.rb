cd9bb86631be7198a1e1ac83c1eca9eab0122f63
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-08-25 09:07:05 +0900

    データを登録できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/' => "home#top"
   get '/about' => "home#about"
   get '/posts/index' => "posts#index"
+  get '/posts/new/' => "posts#new"
+  post '/posts/create' => "posts#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
